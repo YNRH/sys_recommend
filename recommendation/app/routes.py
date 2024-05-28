0b71d7a683f7ea81308afbbd46deb82525ff87f9
YNRH <95716883+YNRH@users.noreply.github.com>
2024-05-28 11:48:34 -0500

    Update routes.py

--- recommendation/app/routes.py
+++ recommendation/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, jsonify, request
 from flask_cors import CORS
-from .recommender import recommend_movies
+from .recommender import recommend_movie
 from .models import User
 from .database import SessionLocal
 
@@ -13,17 +13,12 @@ main_bp = Blueprint('main', __name__)
 def hola():
     return "Hola, probando la ruta de recomendaciones¡"
 
-@main_bp.route('/recommend', methods=['GET'])
-def recommend():
-    recommendations = recommend_movies()
-    return jsonify(recommendations)
-
 @main_bp.route('/recommend/<string:cookie_id>', methods=['GET'])
 def recommend_by_cookie(cookie_id):
     # Aquí debes obtener el user_id basado en el cookie_id
     user_id = get_user_id_by_cookie(cookie_id)
-    recommendations = recommend_movies(user_id=user_id)
-    return jsonify(recommendations)
+    recommendation = recommend_movie(user_id=user_id)
+    return jsonify(recommendation)
 
 def get_user_id_by_cookie(cookie_id):
     db = SessionLocal()
