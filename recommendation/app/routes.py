38ad57e369b41caf10cb151c68cf6367bba7045f
YNRH <yesica.rojas@tecsup.edu.pe>
2024-05-28 22:19:11 -0500

    basic1

--- recommendation/app/routes.py
+++ recommendation/app/routes.py
@@ -14,7 +14,6 @@ main_bp = Blueprint('main', __name__)
 def hola():
     return "Hola, probando la ruta de recomendaciones¡"
 
-<<<<<<< HEAD
 def get_user_id_by_cookie(cookie_id):
     db = SessionLocal()
     try:
@@ -25,16 +24,6 @@ def get_user_id_by_cookie(cookie_id):
 
 @main_bp.route('/recommend/<string:cookie_id>', methods=['GET'])
 def recommend_by_cookie(cookie_id):
-=======
-@main_bp.route('/recommend/<string:cookie_id>', methods=['GET'])
-def recommend_by_cookie(cookie_id):
-    # Aquí debes obtener el user_id basado en el cookie_id
-    user_id = get_user_id_by_cookie(cookie_id)
-    recommendation = recommend_movie(user_id=user_id)
-    return jsonify(recommendation)
-
-def get_user_id_by_cookie(cookie_id):
->>>>>>> 0b71d7a683f7ea81308afbbd46deb82525ff87f9
     db = SessionLocal()
     try:
         user_id = get_user_id_by_cookie(cookie_id)
